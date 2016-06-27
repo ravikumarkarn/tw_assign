2c783c20d353c685853f162823c33f4b8ee7f868
raviktw <ravik@thoughtworks.com>
2016-06-27 10:34:23 +0530

    printing one star

---
+++ star.java
@@ -0,0 +1,14 @@
+class Star_printer{
+void print_star(){
+System.out.println("*");
+}
+void print_line(){
+System.out.println("\n");
+}
+}
+public class star{
+public static void main(String[] args){
+Star_printer s = new Star_printer();
+s.print_star();
+}
+}
