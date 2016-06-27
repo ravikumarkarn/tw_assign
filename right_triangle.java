cddc6c1ee6fd20a88fecda568f8a7b436dcbab82
raviktw <ravik@thoughtworks.com>
2016-06-27 10:59:24 +0530

    printing right angled triangle

---
+++ right_triangle.java
@@ -0,0 +1,12 @@
+public class right_triangle{
+public static void main(String[] arg){
+int n = Integer.parseInt(arg[0]);
+for(int i=1;i<=n;i++)
+{
+for(int j=1;j<=i;j++)
+System.out.print("*");
+System.out.println("");
+}
+}
+}
+
